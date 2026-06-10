d2d0b6421fbcb636651be01d7f0da337b31a01bd
LilliCorleona <malena.kaths@gartenraeume.de>
2026-06-10 13:09:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TagesLog from './tageslog-final.jsx'
+import Questboard from './questboard-final.jsx'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TagesLog />
+    {path === '/questboard' ? <Questboard /> : <TagesLog />}
   </React.StrictMode>
 )
